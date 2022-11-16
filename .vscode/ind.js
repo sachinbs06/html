6da2a5ae5e1208bb6b25466be440215cdb99c065
sachinbs06 <sachin.b.suresh24@gmail.com>
2022-11-16 05:48:12 +0530

    html web designs

---
+++ .vscode/ind.js
@@ -0,0 +1,145 @@
+// var name="hello"
+// console.log(name)
+
+// var a="java"+""+"script"+""+"java"
+// console.log(a)
+
+// var b=(1+8)
+// console.log(b)
+
+// var $h=(20-7)
+// console.log($h);
+
+// console.log("bangalore"-10);
+
+// console.log(isNaN("10"-5));
+
+// console.log(isNaN("HASSAN"-10));
+
+// console.log(1==true);
+
+// console.log(1===true);
+// //null
+// console.log();
+// console.log();
+// var num=null
+// console.log(num);
+
+// var num1;
+// console.log(num1);
+
+// console.log(10>5);
+
+// function greed(){
+//     console.log("happy day..");
+// }
+// greed();//calling function
+// greed();
+// greed();
+// greed();
+
+// function greet(){
+//     return `hello ${use}`
+// }
+// var use="jsp"
+// console.log(greet());
+
+// //iife
+// (function (){
+//     console.log("happy married life");
+// })();
+
+// var add=function(num1,num2){
+//     return(num1+num2)
+// }
+//  var result=add(5,10)
+//  console.log(result);
+
+//  var sub=(num1,num2)=>{
+//      return(num1-num2)
+//  }
+//  var result=sub(5,2)
+//  console.log(result);
+
+//  let array=["hi",87,true,"good"]
+//  console.log(array);
+
+//  let array=[65,34,56,7,9,8]
+//  console.log(array.push(100));
+//  console.log(array);
+
+//  let array=[2,4,5,8,6]
+//  console.log(array.pop());
+//  console.log(array);
+
+// let array=[1,2,3,4,5]
+// console.log(array.shift());
+// console.log(array);
+
+// let array=[1,2,3,4,5]
+// console.log(array.unshift());
+// console.log(array);
+
+// let array=[1,2,3,4,5,6,7,8,9,10]
+// console.log(array.splice(4,5,6));
+// console.log(array);
+
+// let array=[3,2,6,7,1,9,8,5,4]
+// array.sort(function(a,b){return a-b});
+// console.log(array);
+
+// console.log("-----------------------------");
+
+// let number=[3,2,6,7,1,9,8,5,4]
+// number.sort(function(a,b){return b-a});
+// console.log(number);
+
+// let a = [2,4,5,8,6]
+// if(a[0]==06 || a[4]==6){
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+// let b=[6,2,4,5,8]
+// if(b[0]==06||b[4]==6){
+//     console.log(true);
+// }else{
+//     console.log(false)
+// }
+// let person={
+//     name:"raju",
+//     age:"20",
+//     color:"white",
+//     college:"christ",
+//     education:"BE",
+//     place:"mangalore"
+
+// }
+// console.log(person.name);
+// console.log(person.place);
+// console.log(person)
+// person.education="msc"
+// console.log(person.education=50)
+// console.log(person.education,person.age)
+
+// let person={
+//     boy:"preetham",
+//     girl:"preethi",
+//     color:"black",
+//     work:"software",
+// }
+
+// for(let n in person){
+//     console.log(n); //keys
+//     console.log(person[n]);  //values
+//     console.log(n,person[n]);   //keys&values
+
+// }
+
+let array=[]
+let s ="jspiders"
+array[0]=10
+array[10]=50
+for (let n of s)
+console.log(n);
